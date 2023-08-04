6f9575385d52304eb396074b126fedb30dc75d91
Anbolik <141113990+Anbolik@users.noreply.github.com>
2023-08-04 15:23:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let play = document.getElementById("dog");
+      function playMusic() {
+        let audio = new Audio("./sounds/dog.mp3");
+         audio.play()
+      }
+      play.addEventListener("click",playMusic);
+let play1 = document.getElementById("calf");
+      function playMusic1() {
+        let audio1 = new Audio("./sounds/cow.wav");
+         audio1.play()
+      }
+      play1.addEventListener("click",playMusic1);
+let play2 = document.getElementById("elephant");
+      function playMusic2() {
+        let audio2 = new Audio("./sounds/elephant.mp3");
+         audio2.play()
+      }
+      play2.addEventListener("click",playMusic2);
+let play3 = document.getElementById("lion");
+      function playMusic3() {
+        let audio3 = new Audio("./sounds/lion.mp3");
+         audio3.play()
+      }
+      play3.addEventListener("click",playMusic3);
+let play4 = document.getElementById("horse");
+      function playMusic4() {
+        let audio4 = new Audio("./sounds/horse.mp3");
+         audio4.play()
+      }
+      play4.addEventListener("click",playMusic4);
+let play5 = document.getElementById("cat");
+      function playMusic5() {
+        let audio5 = new Audio("./sounds/cat.wav");
+         audio5.play()
+      }
+      play5.addEventListener("click",playMusic5);
